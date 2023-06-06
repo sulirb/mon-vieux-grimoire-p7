4e846c29f1fa3c0542b19cd0cb2b3539cec75cb6
sulirb <121161440+sulirb@users.noreply.github.com>
2023-06-06 11:27:37 +0200

    test

--- api/index.js
+++ api/index.js
@@ -3,6 +3,6 @@ const express = require("express");
 const route = express();
 
 route.use((req, res) => {
-  res.json("On est sur l'api");
+  res.json("On est sur l'api de book");
 });
 module.exports = route;
